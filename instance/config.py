75ddb78ad79d04b2237e22746cf9d4ec58ae1c70
Erick Maina <erick@localhost.localdomain>
2019-09-24 09:49:10 +0300

    setting up application structure and environment

---
+++ instance/config.py
@@ -0,0 +1,22 @@
+""" Configuration file for the API """
+
+class Config(object):
+    """ Parent configuration class """
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    """ Configuration for development environment """
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """ Configuration for the testing environment """
+    TESTING = True
+
+
+app_config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig,
+    'testing': TestingConfig
+}
